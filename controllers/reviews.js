195473a46c74caea6f44d2c9146e016c20ea275d
Subhash Katru <subhashkatru3@gmail.com>
2025-05-01 14:25:01 +0530

    Project file

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,23 +1,40 @@
 const Listing = require("../models/listing.js");
-const Review = require("../models/reviews.js")
+const Review = require("../models/reviews.js");
 
-module.exports.createReview = async(req, res)=>{
-   let listing = await Listing.findById(req.params.id);
-   let newReview = new Review(req.body.review);
-   newReview.author = req.user._id;
-   listing.reviews.push(newReview);
+module.exports.createReview = async (req, res) => {
+    try {
+        let listing = await Listing.findById(req.params.id);
 
-   await newReview.save();
-   await listing.save();
-   req.flash("success", "New Review Created!");
-   res.redirect(`/listings/${listing.id}`);
+        if (!listing) {
+            req.flash("error", "Listing not found!");
+            return res.redirect("/listings");
+        }
+
+        let newReview = new Review(req.body.review);
+        newReview.author = req.user._id;
+        listing.reviews.push(newReview);
+
+        await newReview.save();
+        await listing.save();
+
+        req.flash("success", "New Review Created!");
+        res.redirect(`/listings/${listing.id}`);
+    } catch (error) {
+        req.flash("error", "Something went wrong!");
+        res.redirect(`/listings/${req.params.id}`);
+    }
 };
 
-module.exports.deleteReview = async(req, res)=>{
-    let { id, reviewId } = req.params;
+module.exports.deleteReview = async (req, res) => {
+    try {
+        let { id, reviewId } = req.params;
+        await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+        await Review.findByIdAndDelete(reviewId);
 
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-    res.redirect(`/listings/${id}`);
+        req.flash("success", "Review Deleted!");
+        res.redirect(`/listings/${id}`);
+    } catch (error) {
+        req.flash("error", "Something went wrong!");
+        res.redirect(`/listings/${req.params.id}`);
+    }
 };
